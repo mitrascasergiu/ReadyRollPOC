8319db3efc8c880d30d7d1fd108c47ff8f5bdda3
Sergiu Mitrasca <Mitrascas@kynetix.com>
2019-06-25 14:10:59 +0100

    added new version

---
+++ ReadyRollPOC/Migrations/1.0.1/001_20190625-1408_MitrascaS.sql
@@ -0,0 +1,33 @@
+﻿-- <Migration ID="746588d5-ce31-46cd-80f7-fecd70c76eb4" />
+GO
+
+
+GO
+PRINT N'Altering [dbo].[ib_EquityCash_Select]...';
+
+
+GO
+/*
+Programmer:  Bruce McQuien	
+Description:  Selects cash records from ib_equitycash.
+Date:  28/11/2006
+*/
+
+--test sergiu
+
+ALTER PROC dbo.ib_EquityCash_Select
+@EquityHeaderId int,
+@InternalExternalCode char(1)
+
+AS
+SET NOCOUNT ON
+
+
+SELECT [Description]
+      ,[Amount]
+	  ,[InternalExternalCode] 
+FROM [dbo].[ib_EquityCash]
+WHERE EquityHeaderId = @EquityHeaderId
+AND InternalExternalCode = @InternalExternalCode
+GO
+
